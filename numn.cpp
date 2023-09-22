f94b812a0021ccb4396ee4cc6d539456775891a2
dnhidk <136322609+dnhidk@users.noreply.github.com>
2023-09-22 20:30:32 +0700

    Add files via upload

---
+++ numn.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define dnh int main()
+#define idk ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define open(name) freopen(name".inp","r",stdin) , freopen(name".out","w",stdout);
+#define ll long long
+dnh {
+    idk
+    open("numn");
+    ll n;
+    cin>>n;
+    cout<<(n-1)*3+1;
+    return 0;
+}
